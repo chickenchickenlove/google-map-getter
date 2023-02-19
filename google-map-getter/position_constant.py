50e57145ed87cede5973d23347756be8e87ef370
chickenchickenlove <ojt90902@naver.com>
2023-02-19 23:16:44 +0900

    add position_constant

---
+++ google-map-getter/position_constant.py
@@ -0,0 +1,41 @@
+
+# latitude : > 0 → 북위
+# longitude : > 0 → 동경
+
+MY_DICT = {
+    'HANOI': {
+        'latitude': 21.0090571,
+        'longitude': 105.8607507,
+        'accuracy': 100
+    },
+
+    'HOIAN': {
+        'latitude': 15.8818303,
+        'longitude': 108.3030015,
+        'accuracy': 100
+    },
+
+    'HOCHIMINH': {
+        'latitude': 10.8230989,
+        'longitude': 106.6296638,
+        'accuracy': 100
+    },
+
+    'DANANG': {
+        'latitude': 16.0544068,
+        'longitude': 108.2021667,
+        'accuracy': 100
+    },
+
+    'NATRANG': {
+        'latitude': 11.9980556,
+        'longitude': 109.219167,
+        'accuracy': 100
+    },
+
+    'SAPA': {
+        'latitude': 22.3402,
+        'longitude': 103.844,
+        'accuracy': 100
+    },
+}
